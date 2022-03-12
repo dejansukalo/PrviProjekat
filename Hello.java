d045ecf59b55b498dc4fbcc8f7decb7b3f227648
dejansukalo <101481253+dejansukalo@users.noreply.github.com>
2022-03-12 23:51:10 +0100

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,7 @@
-public class Hello {
-  public static void main (String[] args) {
-  system.out.println("Hello from Github!!!");
-  }
+// Your First Program
+
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); 
+    }
 }
